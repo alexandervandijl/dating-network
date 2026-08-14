aa1dbb9c1bc0fda77a7cc4e93d9d134836260673
alexandervandijl <alex@vandijl.com>
2026-08-14 06:01:10 +0200

    V0.5.0: zet pluginversie en complete bootstrap

--- dating-network/dating-network.php
+++ dating-network/dating-network.php
@@ -1,17 +1,15 @@
 <?php
 /**
  * Plugin Name: Dating Network
- * Description: Veilige datingsite voor singles met wederzijdse matching, interne chat en uitlegbare matchscore.
- * Version: 0.2.0
+ * Description: Veilig datingplatform voor singles met wederzijdse matching, interne chat en uitlegbare matchscore.
+ * Version: 0.5.0
  * Author: Alexander van Dijl
  * Text Domain: dating-network
  */
 
-if (!defined('ABSPATH')) {
-    exit;
-}
+if (!defined('ABSPATH')) { exit; }
 
-define('DN_VERSION', '0.2.0');
+define('DN_VERSION', '0.5.0');
 define('DN_FILE', __FILE__);
 define('DN_DIR', plugin_dir_path(__FILE__));
 define('DN_URL', plugin_dir_url(__FILE__));
@@ -26,12 +24,10 @@ require_once DN_DIR . 'includes/class-dn-updater.php';
 
 register_activation_hook(__FILE__, ['DN_Install', 'activate']);
 
-add_action('plugins_loaded', static function () {
+add_action('plugins_loaded', static function (): void {
     DN_Install::maybe_upgrade();
     DN_Updater::init();
     DN_Core::init();
     DN_Shortcodes::init();
-    if (is_admin()) {
-        DN_Admin::init();
-    }
+    if (is_admin()) { DN_Admin::init(); }
 });
